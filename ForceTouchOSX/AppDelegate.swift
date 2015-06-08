c41b3217febadb4716ad6dd18d5b2fc3da8660ea
banjun <banjun@gmail.com>
2015-06-08 14:06:13 +0900

    accept force click using NSButton and show its value to label

--- ForceTouchOSX/AppDelegate.swift
+++ ForceTouchOSX/AppDelegate.swift
@@ -7,21 +7,45 @@
 //
 
 import Cocoa
+import NorthLayout
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     @IBOutlet weak var window: NSWindow!
-
-
+     
+    let button = NSButton(frame: NSZeroRect)
+    let label = NSTextField(frame: NSZeroRect)
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        let contentView = window.contentView as! NSView
+        button.bezelStyle = .ThickSquareBezelStyle
+        button.title = "Feel the Force"
+        button.setButtonType(NSButtonType.MultiLevelAcceleratorButton)
+        button.maxAcceleratorLevel = 5
+        button.target = self
+        button.action = "clicked:"
+        
+        label.font = NSFont.systemFontOfSize(36)
+        label.textColor = NSColor.blackColor()
+        label.drawsBackground = false
+        label.bordered = false
+        label.bezeled = false
+        label.selectable = false
+        label.editable = false
+        label.alignment = .CenterTextAlignment
+        
+        let autolayout = contentView.northLayoutFormat([:], ["button": button, "label": label])
+        autolayout("H:|-[button]-|")
+        autolayout("H:|-[label]-|")
+        autolayout("V:|-[button]-[label]")
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+    
+    @objc private func clicked(sender: AnyObject?) {
+        let level = button.integerValue
+        let percentage = Float(level) / Float(button.maxAcceleratorLevel)
+        
+        label.stringValue = "level = \(level), \(percentage * 100) %"
+        label.font = NSFont.systemFontOfSize(CGFloat(24 + 24 * percentage))
     }
-
-
 }
 
