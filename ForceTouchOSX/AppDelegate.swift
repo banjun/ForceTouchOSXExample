3d188f60af65292a18dec20b77d4c1f3c8b6045d
banjun <banjun@gmail.com>
2015-06-08 13:30:30 +0900

    Initial Commit

---
+++ ForceTouchOSX/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ForceTouchOSX
+//
+//  Created by BAN Jun on 6/8/15.
+//  Copyright (c) 2015 banjun. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
